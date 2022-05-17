c481634e83324d9dfa5a103c455b68a897b7f6c3
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-05-17 13:40:26 -0500

    adds nav bar and dropdown menus

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-content">
+      <div className="login-form">
+
+      </div>
+      <div className="signup-form">
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
