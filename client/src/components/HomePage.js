3ba835e48e353f1121e79709fca4fe08d4cbefeb
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-05-18 12:54:41 -0500

    working on homepage/loginpage css structuring

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import Hero from "../images/bountyHunter.jpg";
+import LoginForm from './LoginForm.js';
+
+const HomePage = () => {
+  return (
+    <div className="content-window">
+      <LoginForm />
+      
+      
+      
+    </div>
+    
+      
+
+  );
+};
+
+export default HomePage;
